4604268477ac18f3780bd8a4ad7b9c6de3b2b18f
Madzhar <t_mad@mail.ru>
2022-12-08 20:32:39 +0300

    Added seminars

---
+++ seminar 3/task 17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.Write("Enter X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("I quarter!");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("II quarter!");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("III quarter!");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("IV quarter");
+}
+else Console.WriteLine("ERROR!!!");
---
+++ seminar 3/task 18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Enter quarter -> ");
+int q = Convert.ToInt32(Console.ReadLine());
+
+if (q == 1)
+{
+    Console.Write("x > 0; y > 0");
+}
+else if (q == 2)
+{
+    Console.Write("x < 0; y > 0");
+}
+else if (q == 3)
+{
+    Console.Write("x < 0; y < 0");
+}
+else if (q == 4)
+{
+    Console.Write("x > 0; y < 0");
+}
+else Console.WriteLine("ERROR!");
